4304eeae6bfe6cd81eb8554b0715ebffd9b0afff
AyeshaNoorKhan <100527908+AyeshaNoorKhan@users.noreply.github.com>
2022-08-29 10:38:34 -0700

    loader initial job listing page

--- src/components/JobListingPage/JobList.js
+++ src/components/JobListingPage/JobList.js
@@ -1,39 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-import Job from './Job';
-import SearchSort from './SearchSort';
-
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import Job from "./Job";
+import SearchSort from "./SearchSort";
 
 // function JobList() {
 function JobList() {
-    const [joblist, setJobList] = useState([]);
-    const [filteredJobs, setFilteredJobs] = useState([]);
+  const [joblist, setJobList] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        Axios.get("https://atsbackend.herokuapp.com/api/job/getjobs")
-            .then((response) => {
-                const jobData = response.data.getAllJob;
-                setJobList(jobData);
-                setFilteredJobs(jobData);
-            })
-            .catch(error => {
-                console.error("Error fetching data: ", error);
-            })
-
-    }, [])
-
+  useEffect(() => {
+    Axios.get("https://atsbackend.herokuapp.com/api/job/getjobs")
+      .then((response) => {
+        const jobData = response.data.getAllJob;
+        setTimeout(() => {
+          setJobList(jobData);
+          setFilteredJobs(jobData);
+          setLoading(false);
+        }, 3000);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+  if (isLoading) {
+    return "Loading";
+  } else {
     return (
-        <div className="joblist"><SearchSort joblist={joblist} filteredJobs={filteredJobs} setFilteredJobs={setFilteredJobs} />
-            {
-                filteredJobs.map((job, i) =>
-                    <Job
-                        key={i}
-                        job={job}
-                    />
-                )
-            }
-        </div>
-    )
+      <div className="joblist">
+        <SearchSort
+          joblist={joblist}
+          filteredJobs={filteredJobs}
+          setFilteredJobs={setFilteredJobs}
+        />
+        {filteredJobs.map((job, i) => (
+          <Job key={i} job={job} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default JobList
+export default JobList;
